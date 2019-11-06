4ab69cb85d28f189cf8a88236aaf3e3f3663c127
Cane Rosso <astor@usa.com>
2019-11-06 07:25:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,11 @@
 # My Code here....
 
 #map-like functions
-define 
-array = []
-map_to_negativize = -1*array
+define map_to_negativize(source_array)
+
+map_to_negativize = -1*source_array
+end
+
+
 
 #reduce-like
