b60ff7ded008e81c3c7f06de5779150d6e134098
Cane Rosso <astor@usa.com>
2019-11-06 07:35:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,10 +6,10 @@ def map_to_negativize(source_array)
   new = []
   i = 0
   while i < source_array.length
-    new.push(yield(source_array[i]))
+    new.push(yield(-1*source_array[i]))
     i += 1
   end
-  new = -1*source_array
+  new 
 end
 
 
