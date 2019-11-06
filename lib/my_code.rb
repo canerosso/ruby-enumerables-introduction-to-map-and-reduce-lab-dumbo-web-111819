299f0986e06cbd99bf138cd3b12a94a1d680cba9
Cane Rosso <astor@usa.com>
2019-11-06 07:34:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,22 +9,9 @@ def map_to_negativize(source_array)
     new.push(yield(source_array[i]))
     i += 1
   end
-  new
+  new = -1*source_array
 end
 
 
 #reduce-like
-def reduce(s, sp=nil)
-  if sp
-    accum = sp
-    i = 0
-  else
-    accum = s[0]
-    i = 1
-  end
-  while i < s.length
-    accum = yield(accum, s[i])
-    i += 1
-  end
-  accum
-end
+
